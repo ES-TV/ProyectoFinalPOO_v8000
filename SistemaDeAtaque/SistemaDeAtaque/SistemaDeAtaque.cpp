64e2aed727370d8f50e8553ef0fe46d3f50e460c
STV <estebanchvzal@gmail.com>
2026-05-17 14:02:42 -0600

    ⚡️Feat: Limpiar main e implementar impresion de texto

--- SistemaDeAtaque/SistemaDeAtaque/SistemaDeAtaque.cpp
+++ SistemaDeAtaque/SistemaDeAtaque/SistemaDeAtaque.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <random>
+#include <thread> // esta libreria es para usar la funcion sleep_for y hacer pausas en el programa
+#include <chrono> // esta libreria es para usar la funcion seconds y hacer pausas
 
 class Personaje // Esta va a ser la clase Padre/Base de los personajes
 {
@@ -49,14 +51,16 @@ public:
 		return danio;
 	}
 
+	std::string fn_GetNombre()
+	{
+		return nombre;
+	}
 };
 
 class Jugador : public Personaje // Clase Jugador que heredara cosas de la principal ya le decimos que va a ser Heredera de la clase Padre/Base
 {
 protected:
-	std::string nombrePersonaje = "Zawardo";
-	int vidaPersonaje = 40;
-	int danioPresonaje = 10;
+
 
 
 public:
@@ -64,60 +68,74 @@ public:
 		Personaje(p_nombreJugador, p_vidaJugador, p_danioJugador) // Asi es como se declara el constructor, solo que con los mismos parametros de de esta clase
 		// (Se pudieron poner los constructores en la misma fila pero para los comentarios os separe)
 	{
-		p_nombreJugador = nombrePersonaje;
-		p_vidaJugador = vidaPersonaje;
-		p_danioJugador = danioPresonaje;
+
 	}
+
+
 };
 
 class Enemigo : public Personaje
 {
 public:
 	// Aqui trate de hacer vectores con varios enemigos pero no supe como hacerlo, asi que hice 3 objetos de la clase Enemigo
-	std::string nombreEnemigo;
-	int vidaEnemigo;
-	int danioEnemigo;
+
 
 	Enemigo(std::string p_nombreEnemy, int p_vidaEnemy, int p_danioEnemy) :
 		Personaje(p_nombreEnemy, p_vidaEnemy, p_danioEnemy)
 	{
-		nombreEnemigo = p_nombreEnemy;
-		vidaEnemigo = p_vidaEnemy;
-		danioEnemigo = p_danioEnemy;
+
 	}
 };
 
-int main()
+// Aqui se crean los objetos como variables globales para poder usarlas en cualquier parte del codigo
+Jugador objJugador("Zawardo", 40, 10);
+Enemigo objEnemy1("Chaneque", 20, 3);
+Enemigo objEnemy2("Bruja", 30, 5);
+Enemigo objEnemy3("Nahual", 40, 7);
+Enemigo enemigos[] = { objEnemy1, objEnemy2, objEnemy3 }; // Se genero un vector con los obejots de enemigos
+Enemigo objEnemy = enemigos[std::rand() % 3]; // Ya se hace el random para pelear
+
+void fn_EfectoTexto(std::string texto) // Esta funcion es para imprimir el texto con un efecto de juego  que se imprime letra por letra (Se usara en ve del std::cout)
 {
-	std::srand(time(NULL));
+	for (char c : texto)
+	{
+		std::cout << c << std::flush; // Imprime cada caracter y vacia el buffer para que se vea el efecto de escritura
+		std::this_thread::sleep_for(std::chrono::milliseconds(50)); // Pausa de 50 milisegundos entre cada caracter
+	}
+	std::cout << std::endl; // Salto de linea al final del texto
+}
 
-	Jugador objJugador("Zawardo", 40, 10);
-	Enemigo objEnemy1("Chaneque", 20, 3);
-	Enemigo objEnemy2("Bruja", 30, 5);
-	Enemigo objEnemy3("Nahual", 40, 7);
+void fn_ImprimirTextoPelea()
+{
 
-	Enemigo enemigos[] = { objEnemy1, objEnemy2, objEnemy3 }; // Se genero un vector con los obejots de enemigos
-	Enemigo objEnemy = enemigos[std::rand() % 3]; // Ya se hace el random para pelear
-
-	std::cout << "Te has encontrado con un " << objEnemy.nombreEnemigo << std::endl;
-	std::cout << "Sus stats son: " << "Vida: " << objEnemy.fn_getVida() << " Danio: " << objEnemy.fn_GetDanio() << std::endl;
+	fn_EfectoTexto("Te has encontrado con un " + objEnemy.fn_GetNombre());
+	fn_EfectoTexto("Sus stats son:\n Vida: " + std::to_string(objEnemy.fn_getVida()) + " Danio = " + std::to_string(objEnemy.fn_GetDanio()));
+	// Se usa el std::to_string() para convertir los numeros a string para imprimirlos igual evitando errores matematicos
 	std::system("pause");
 	// Atacar
 	do
 	{
 		int danioJugador = objJugador.fn_GetDanio();
 		objEnemy.fn_Attack(danioJugador);
-		std::cout << "Zawardo pega un machetazo haciendole " << danioJugador << " de danio al enemigo" << std::endl;
-		std::cout << "Vida del enemigo: " << objEnemy.fn_getVida() << std::endl; // Aqui checamos la vida del enemigo
+		fn_EfectoTexto("Zawardo pega un machetazo haciendole " + std::to_string(danioJugador) + " de danio al enemigo");
+		fn_EfectoTexto("Vida del enemigo: " + std::to_string(objEnemy.fn_getVida())); // Aqui checamos la vida del enemigo
 		std::system("pause");
 		int danioEnemy = objEnemy.fn_GetDanio();
 		objJugador.fn_Attack(danioEnemy);
-		std::cout << "El enemigo te ataca dandote " << danioEnemy << " de danio" << std::endl;
-		std::cout << "Vida: " << objJugador.fn_getVida() << std::endl; // Aqui checamos la vida del jugador
+		fn_EfectoTexto("El enemigo te ataca dandote " + std::to_string(danioEnemy) + " de danio");
+		fn_EfectoTexto("Vida: " + std::to_string(objJugador.fn_getVida())); // Aqui checamos la vida del jugador
 		std::system("pause");
 		std::system("cls");
 	} while (objJugador.fn_estavivo() && objEnemy.fn_estavivo());
-	std::cout << "El enemigo ha sido derrotado" << std::endl;
+	fn_EfectoTexto("El enemigo ha sido derrotado");
+}
+
+
+int main()
+{
+	std::srand(time(NULL));
+
+	fn_ImprimirTextoPelea();
 	
 	return 0;
 }
